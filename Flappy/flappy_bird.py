a1b27e8250489070073e44af8d44dffec177cab6
pasqualeiuliano <pi16348@essex.ac.uk>
2020-10-26 17:44:45 +0000

    Update

--- Flappy/flappy_bird.py
+++ Flappy/flappy_bird.py
@@ -180,8 +180,18 @@ def draw_window(win, bird, pipes, base, score):
 	pygame.display.update()
 
 
-def main():
-	bird = Bird(230, 350)
+def main(genomes, config):
+	nets = []
+	ge = []
+	birds = []
+
+	for g in genomes:
+		net = neat.nn.FeedForwardNetwork(g, config)
+		nets.append(net)
+		birds.append(Bird(230, 350))
+		g.fitness = 0
+		ge.append(g)
+
 	base = Base(730)
 	pipes = [Pipe(600)]
 	score = 0
@@ -200,25 +210,38 @@ def main():
 		rem = []
 		add_pipe = False
 		for pipe in pipes:
-			if pipe.collide(bird):
-				pass
+			for x, bird in enumerate(birds):
+				if pipe.collide(bird):
+					ge[x].fitness -= 1
+					birds.pop(x)
+					nets.pop(x)
+					ge.pop(x)
+
+				if not pipe.passed and pipe.x < bird.x:
+					pipe.passed = True
+					add_pipe = True
 
 			if pipe.x + pipe.PIPE_TOP.get_width() < 0:
 				rem.append(pipe)
-
-			if not pipe.passed and pipe.x < bird.x:
-				pipe.passed = True
-				add_pipe = True
 
 			pipe.move()
 
 		if add_pipe:
 			score += 1
+
+			for g in ge:
+				g.fitness += 5
+
 			pipes.append(Pipe(600))
 
 		for r in rem:
 			pipes.remove(r)
 			
+		for x, bird in enumerate(birds):
+			if bird.y + bird.img.get_height() >= 730:
+				birds.pop(x)
+				nets.pop(x)
+				ge.pop(x)
 
 		#bird.move()
 		base.move()
@@ -228,3 +251,28 @@ def main():
 	quit()
  
 main()
+
+def run(config_path):
+	config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
+		neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+	population = neat.Population(config)
+
+	population.add_reporter(neat.StdOutReporter(True))
+	stats = neat.StatisticsReporter()
+	population.add_reporter(stats)
+
+	winner = population.run(main, 50)
+
+
+
+if __name__ == "__main__":
+	local_dir = os.path.dirname(__file__)
+	config_path = os.path.join(local_dir, "config-feedforward.txt")
+	run(config_path)
+
+
+
+
+
+
